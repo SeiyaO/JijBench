e354dbf98ba0af0f8756e69267cef657d08311f6
j-i-k-o <jiko.rogy@gmail.com>
2022-02-07 15:06:35 +0900

    add github action

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@ from setuptools import setup
 
 setup(
     name="jijbench",
-    version="0.0.1",
     python_requires=">=3.9, <3.11",
     packages=["jijbench", "jijbench.problems"],
+    version_config={
+         "template": "{tag}",
+         "dirty_template": "{tag}",
+    },
+    setup_requires=[
+        'setuptools-git-versioning',
+    ],
     install_requires=[
         "jijzept",
         "jijmodeling",
