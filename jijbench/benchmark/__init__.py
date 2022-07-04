4f3ea53c56412ae464c7ed001ef665ed5f3b4c60
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-07-05 06:38:31 +0900

    Update __init__.py

--- jijbench/benchmark/__init__.py
+++ jijbench/benchmark/__init__.py
@@ -1,3 +1,8 @@
 from pkgutil import extend_path
 
 __path__ = extend_path(__path__, __name__)
+
+from jijbench.benchmark.benchmark import Benchmark
+from jijbench.benchmark.validation import on_solver, on_problem, on_instance_data
+
+__all__ = []
