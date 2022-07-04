923e847ae02a32ad15d0e0ffe2f9dab3a0a54feb
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-07-05 07:15:25 +0900

    Update __init__.py

--- jijbench/experiment/__init__.py
+++ jijbench/experiment/__init__.py
@@ -2,7 +2,7 @@ from pkgutil import extend_path
 
 __path__ = extend_path(__path__, __name__)
 
-from jijbench.experiment import Experiment
+from jijbench.experiment.experiment import Experiment
 import jijbench.experiment._parser
 
 __all__ = []
