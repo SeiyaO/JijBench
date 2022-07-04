198462a84cd2678b6db525a962cf52b125167a83
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-07-05 07:28:52 +0900

    Update __init__.py

--- jijbench/__init__.py
+++ jijbench/__init__.py
@@ -5,6 +5,6 @@ __path__ = extend_path(__path__, __name__)
 from jijbench.benchmark.benchmark import Benchmark
 from jijbench.evaluation.evaluation import Evaluator
 from jijbench.experiment import Experiment
-from jijbench.problem import problem, get
-
+from jijbench.problem import problem
+from jijbench.problem.get import get_instance_data, get_problem
 __all__ = []
