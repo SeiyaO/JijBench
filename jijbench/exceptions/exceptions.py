042bba5ebbc46fcb5b1e7a2d63048bb9c9a4069b
tatsuyagoto <t.goto@j-ij.com>
2022-12-20 07:58:29 +0000

    make exceptions class

---
+++ jijbench/exceptions/exceptions.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+
+class JijBenchmarkError(Exception):
+    """
+    Exception for JijBenchmark Errors related to JijBenchmark inherit from this.
+    Exception class.
+    """
