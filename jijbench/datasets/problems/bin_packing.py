eb4f45575b4d74893bcceae84f70ce8ae20ece91
tatsuyagoto <t.goto@j-ij.com>
2023-06-22 12:19:56 +0000

    update jijmodeling version and use the new interface

--- jijbench/datasets/problems/bin_packing.py
+++ jijbench/datasets/problems/bin_packing.py
@@ -4,19 +4,19 @@ import jijmodeling as jm
 
 
 def bin_packing():
-    w = jm.Placeholder("w", dim=1)
+    w = jm.Placeholder("w", ndim=1)
     num_items = jm.Placeholder("n")
     c = jm.Placeholder("c")
 
     # y[j]: bin j を使用するかしないか
-    y = jm.Binary("y", shape=(num_items,))
+    y = jm.BinaryVar("y", shape=(num_items,))
     # x[i][j]: item i を bin j に入れるとき1
-    x = jm.Binary("x", shape=(num_items, num_items))
+    x = jm.BinaryVar("x", shape=(num_items, num_items))
 
     # i: itemの添字
-    i = jm.Element("i", num_items)
+    i = jm.Element("i", belong_to=num_items)
     # j: binの添字
-    j = jm.Element("j", num_items)
+    j = jm.Element("j", belong_to=num_items)
 
     problem = jm.Problem("bin-packing")
 
@@ -25,12 +25,12 @@ def bin_packing():
     problem += obj
 
     # Constraint1: 各itemをちょうど1つのbinにぶち込む
-    const1 = jm.Constraint("onehot-constraint", jm.Sum(j, x[i, j]) - 1 == 0, forall=i)
+    const1 = jm.Constraint("onehot-constraint", jm.sum(j, x[i, j]) - 1 == 0, forall=i)
     problem += const1
 
     # Constraint2: knapsack制約
     const2 = jm.Constraint(
-        "knapsack-constraint", jm.Sum(i, w[i] * x[i, j]) - y[j] * c <= 0, forall=j
+        "knapsack-constraint", jm.sum(i, w[i] * x[i, j]) - y[j] * c <= 0, forall=j
     )
     problem += const2
 
--- jijbench/datasets/problems/travelling_salesman_with_time_windows.py
+++ jijbench/datasets/problems/travelling_salesman_with_time_windows.py
@@ -8,23 +8,23 @@ def travelling_salesman_with_time_windows():
     problem = jm.Problem("travelling-salesman-with-time-windows")
 
     # 距離行列
-    dist = jm.Placeholder("d", dim=2)  # 距離行列
+    dist = jm.Placeholder("d", ndim=2)  # 距離行列
     N = jm.Placeholder("N")
     e = jm.Placeholder("e", shape=(N,))  # ready time
     l = jm.Placeholder("l", shape=(N,))  # due time
-    x = jm.Binary("x", shape=(N, N))
-    t = jm.Integer("t", shape=(N,), lower=e, upper=l)
+    x = jm.BinaryVar("x", shape=(N, N))
+    t = jm.IntegerVar("t", shape=(N,), lower_bound=e, upper_bound=l)
 
-    i = jm.Element("i", N)
-    j = jm.Element("j", N)
+    i = jm.Element("i", belong_to=N)
+    j = jm.Element("j", belong_to=N)
 
     # Objevtive Function: 距離の最小化
     sum_list = [i, (j, j != i)]
-    obj = jm.Sum(sum_list, dist[i, j] * x[i, j])
+    obj = jm.sum(sum_list, dist[i, j] * x[i, j])
     problem += obj
 
     # Const1: 都市iから出る辺は1つ
-    term1 = jm.Sum((j, j != i), x[i, j])
+    term1 = jm.sum((j, j != i), x[i, j])
     problem += jm.Constraint(
         "onehot-constraint1",
         term1 == 1,
@@ -34,7 +34,7 @@ def travelling_salesman_with_time_windows():
     )
 
     # Const2: 都市iに入る辺は1つ
-    term2 = jm.Sum((j, j != i), x[j, i])
+    term2 = jm.sum((j, j != i), x[j, i])
     problem += jm.Constraint(
         "onehot-constraint2",
         term2 == 1,
